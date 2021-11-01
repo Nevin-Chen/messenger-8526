cbdf60b8e5eda473d31dec83469592ad299d37e5
nevin-chen <nevinchen6@gmail.com>
2021-11-01 13:56:50 -0400

    implements designs for auth pages

--- client/src/Login.js
+++ client/src/Login.js
@@ -8,10 +8,68 @@ import {
   Button,
   FormControl,
   TextField,
+  CardMedia,
+  InputAdornment,
 } from "@material-ui/core";
 import { login } from "./store/utils/thunkCreators";
+import { makeStyles } from "@material-ui/core/styles";
+import sideBackground from "./assets/images/bg-img.png";
+import bubbleImage from "./assets/images/bubble.svg";
+
+const useStyles = makeStyles(() => ({
+  authNav: {
+    display: "flex",
+    justifyContent: "flex-end",
+    flexDirection: "row",
+    padding: "2rem",
+    top: 0,
+    right: 0,
+    "@media (max-width:599px)": {
+      justifyContent: "center",
+    },
+  },
+  createAccountButton: {
+    border: "none",
+    boxShadow: "0 0 15px rgba(0, 0, 0, 0.1)",
+    "&:hover": {
+      border: "none",
+    },
+  },
+  sideBackground: {
+    width: "100%",
+    maxheight: "auto",
+    minHeight: "100vh",
+  },
+  overlayFrame: {
+    position: "relative",
+    "@media (max-width:599px)": {
+      display: "none",
+    },
+  },
+  overlayBackground: {
+    backgroundColor: "#86B9FF",
+    backgroundImage: "linear-gradient(to bottom, #3A8DFF, #86B9FF)",
+    position: "absolute",
+    top: 0,
+    height: "100%",
+    width: "100%",
+    textAlign: "center",
+    opacity: "85%",
+    display: "grid",
+    alignItems: "center",
+  },
+  authFormContainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  authForm: {
+    width: "60%",
+    marginTop: "5%",
+  },
+}));
 
 const Login = (props) => {
+  const classes = useStyles();
   const history = useHistory();
   const { user, login } = props;
 
@@ -28,40 +86,79 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Button onClick={() => history.push("/register")}>Register</Button>
-        </Grid>
-        <form onSubmit={handleLogin}>
+    <Grid container root>
+      <Grid className={classes.overlayFrame} sm={3} md={4}>
+        <CardMedia
+          className={classes.sideBackground}
+          component="img"
+          image={sideBackground}
+          alt="side-img"
+        />
+        <Box className={classes.overlayBackground}>
           <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
+            <Box
+              src={bubbleImage}
+              alt="text-bubble"
+              component="img"
+              sx={{ marginBottom: "8%", width: "15%", height: "15%" }}
+            />
+            <Box>
+              <Typography variant="h1">Converse with anyone</Typography>
+              <Typography variant="h1">with any language</Typography>
+            </Box>
+          </Grid>
+        </Box>
+      </Grid>
+      <Grid xs={12} sm={9} md={8}>
+        <Box className={classes.authNav}>
+          <Typography variant="h3">Don't have an account?</Typography>
+          <Button
+            className={classes.createAccountButton}
+            variant="outlined"
+            onClick={() => history.push("/register")}
+            color="primary"
+          >
+            <Typography variant="button">Create account</Typography>
+          </Button>
+        </Box>
+        <Grid className={classes.authFormContainer}>
+          <Box className={classes.authForm}>
+            <Typography variant="h2">Welcome back!</Typography>
+            <form onSubmit={handleLogin}>
+              <FormControl margin="normal" required fullWidth>
                 <TextField
                   aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
+                  label="E-mail Address"
+                  name="email"
+                  type="email"
                 />
               </FormControl>
-            </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Box>
+              <FormControl margin="normal" required fullWidth>
+                <TextField
+                  label="Password"
+                  aria-label="password"
+                  type="password"
+                  name="password"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="start">
+                        <Typography color="primary">Forgot?</Typography>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </FormControl>
+              <Box
+                sx={{ display: "flex", justifyContent: "center", mt: "15%" }}
+              >
+                <Button type="submit" variant="contained" color="primary">
+                  <Typography variant="button">Login</Typography>
+                </Button>
+              </Box>
+            </form>
+          </Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -9,10 +9,67 @@ import {
   FormControl,
   TextField,
   FormHelperText,
+  CardMedia,
 } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
+import { makeStyles } from "@material-ui/core/styles";
+import sideBackground from "./assets/images/bg-img.png";
+import bubbleImage from "./assets/images/bubble.svg";
+
+const useStyles = makeStyles(() => ({
+  authNav: {
+    display: "flex",
+    justifyContent: "flex-end",
+    flexDirection: "row",
+    padding: "2rem",
+    top: 0,
+    right: 0,
+    "@media (max-width:599px)": {
+      justifyContent: "center",
+    },
+  },
+  createAccountButton: {
+    border: "none",
+    boxShadow: "0 0 15px rgba(0, 0, 0, 0.1)",
+    "&:hover": {
+      border: "none",
+    },
+  },
+  sideBackground: {
+    width: "100%",
+    maxheight: "auto",
+    minHeight: "100vh",
+  },
+  overlayFrame: {
+    position: "relative",
+    "@media (max-width:599px)": {
+      display: "none",
+    },
+  },
+  overlayBackground: {
+    backgroundColor: "#86B9FF",
+    backgroundImage: "linear-gradient(to bottom, #3A8DFF, #86B9FF)",
+    position: "absolute",
+    top: 0,
+    height: "100%",
+    width: "100%",
+    textAlign: "center",
+    opacity: "85%",
+    display: "grid",
+    alignItems: "center",
+  },
+  authFormContainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  authForm: {
+    width: "60%",
+    marginTop: "5%",
+  },
+}));
 
 const Login = (props) => {
+  const classes = useStyles();
   const history = useHistory();
   const { user, register } = props;
   const [formErrorMessage, setFormErrorMessage] = useState({});
@@ -38,15 +95,45 @@ const Login = (props) => {
 
   return (
     <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Button onClick={() => history.push("/login")}>Login</Button>
-        </Grid>
-        <form onSubmit={handleRegister}>
+      <Grid className={classes.overlayFrame} sm={3} md={4}>
+        <CardMedia
+          className={classes.sideBackground}
+          component="img"
+          image={sideBackground}
+          alt="side-img"
+        />
+        <Box className={classes.overlayBackground}>
           <Grid>
-            <Grid>
-              <FormControl>
+            <Box
+              src={bubbleImage}
+              alt="text-bubble"
+              component="img"
+              sx={{ marginBottom: "8%", width: "15%", height: "15%" }}
+            />
+            <Box>
+              <Typography variant="h1">Converse with anyone</Typography>
+              <Typography variant="h1">with any language</Typography>
+            </Box>
+          </Grid>
+        </Box>
+      </Grid>
+      <Grid xs={12} sm={9} md={8}>
+        <Box className={classes.authNav}>
+          <Typography variant="h3">Already have an account?</Typography>
+          <Button
+            className={classes.createAccountButton}
+            variant="outlined"
+            onClick={() => history.push("/login")}
+            color="primary"
+          >
+            Login
+          </Button>
+        </Box>
+        <Grid className={classes.authFormContainer}>
+          <Box className={classes.authForm}>
+            <Typography variant="h2">Create an account.</Typography>
+            <form onSubmit={handleRegister}>
+              <FormControl margin="normal" required fullWidth>
                 <TextField
                   aria-label="username"
                   label="Username"
@@ -55,9 +142,7 @@ const Login = (props) => {
                   required
                 />
               </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl>
+              <FormControl margin="normal" required fullWidth>
                 <TextField
                   label="E-mail address"
                   aria-label="e-mail address"
@@ -66,9 +151,12 @@ const Login = (props) => {
                   required
                 />
               </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
+              <FormControl
+                error={!!formErrorMessage.confirmPassword}
+                margin="normal"
+                required
+                fullWidth
+              >
                 <TextField
                   aria-label="password"
                   label="Password"
@@ -81,9 +169,12 @@ const Login = (props) => {
                   {formErrorMessage.confirmPassword}
                 </FormHelperText>
               </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
+              <FormControl
+                error={!!formErrorMessage.confirmPassword}
+                margin="normal"
+                required
+                fullWidth
+              >
                 <TextField
                   label="Confirm Password"
                   aria-label="confirm password"
@@ -96,13 +187,17 @@ const Login = (props) => {
                   {formErrorMessage.confirmPassword}
                 </FormHelperText>
               </FormControl>
-            </Grid>
-            <Button type="submit" variant="contained" size="large">
-              Create
-            </Button>
-          </Grid>
-        </form>
-      </Box>
+              <Box
+                sx={{ display: "flex", justifyContent: "center", mt: "15%" }}
+              >
+                <Button type="submit" variant="contained" color="primary">
+                  <Typography variant="button">Create</Typography>
+                </Button>
+              </Box>
+            </form>
+          </Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
