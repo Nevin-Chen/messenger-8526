63a9d64e6d0faf26431880f63d499be40e236c44
nevin-chen <nevinchen6@gmail.com>
2021-11-06 00:29:44 -0400

    implements the ability to attach images to messages

--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -1,41 +1,108 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Grid } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
     flexDirection: "column",
-    alignItems: "flex-end"
+    alignItems: "flex-end",
   },
   date: {
     fontSize: 11,
     color: "#BECCE2",
     fontWeight: "bold",
-    marginBottom: 5
+    marginBottom: 5,
   },
   text: {
     fontSize: 14,
     color: "#91A3C0",
     letterSpacing: -0.2,
     padding: 8,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   bubble: {
     background: "#F4F6FA",
-    borderRadius: "10px 10px 0 10px"
-  }
+    borderRadius: "10px 10px 0 10px",
+  },
+  attachments: {
+    borderRadius: "10px 10px 0 10px",
+  },
 }));
 
 const SenderBubble = (props) => {
   const classes = useStyles();
-  const { time, text } = props;
+  const { time, text, attachments } = props;
+  console.log(attachments, "asjdiojadss");
   return (
     <Box className={classes.root}>
       <Typography className={classes.date}>{time}</Typography>
-      <Box className={classes.bubble}>
-        <Typography className={classes.text}>{text}</Typography>
-      </Box>
+
+      {text && !attachments && (
+        <Box className={classes.bubble}>
+          <Typography className={classes.text}>{text}</Typography>
+        </Box>
+      )}
+
+      {attachments && !text && (
+        <Grid container direction="row" justifyContent="flex-end" spacing={1}>
+          {attachments.map((image) => (
+            <Grid item>
+              <img
+                className={classes.attachments}
+                src={image}
+                alt="text-attachment"
+                height={attachments.length > 1 ? "100px" : "180px"}
+                width={attachments.length > 1 ? "120px" : "200px"}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+
+      {attachments && text && attachments.length === 1 && (
+        <Box sx={{ position: "relative", textAlign: "center" }}>
+          <img
+            className={classes.attachments}
+            src={attachments[0]}
+            alt="text-attachment"
+            height="180px"
+            width="200px"
+          />
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: 5,
+              borderRadius: "0 0 0 10px",
+              background: "#F4F6FA",
+              width: "100%",
+            }}
+          >
+            <Typography className={classes.text}>{text}</Typography>
+          </Box>
+        </Box>
+      )}
+
+      {attachments && text && attachments.length > 1 && (
+        <Fragment>
+          <Box className={classes.bubble} sx={{ mb: 1 }}>
+            <Typography className={classes.text}>{text}</Typography>
+          </Box>
+          <Grid container direction="row" justifyContent="flex-end" spacing={1}>
+            {attachments.map((image) => (
+              <Grid item>
+                <img
+                  className={classes.attachments}
+                  src={image}
+                  alt="text-attachment"
+                  height="100px"
+                  width="120px"
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Fragment>
+      )}
     </Box>
   );
 };
